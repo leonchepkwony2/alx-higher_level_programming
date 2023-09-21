2eeaaf8eb12978919fe8345e8ec005379ac2f4ab
leon chepkwony <leonchepkwony2@gmail.com>
2023-09-21 15:41:55 +0300

    fixed an indentation bug

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,18 +5,16 @@
 class Base:
     """ Base class for managing id attribute """
 
+    __nb_objects = 0
 
-__nb_objects = 0
+    def __init__(self, id=None):
+        """ Constructor method """
 
-
-def __init__(self, id=None):
-    """ Constructor method """
-
-    # If id is provided, set it as the instance's id
-    if id is not None:
-        self.id = id
-    else:
-        # If id is not provided, increment the class attribute
-        # __nb_objects and set it as the instance's id
-        Base.__nb_objects += 1
-        self.id = Base.__nb_objects
+        # If id is provided, set it as the instance's id
+        if id is not None:
+            self.id = id
+        else:
+            # If id is not provided, increment the class attribute
+            # __nb_objects and set it as the instance's id
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
