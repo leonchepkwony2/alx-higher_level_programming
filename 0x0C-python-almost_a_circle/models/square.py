fbde3df22163cf798e3d4a2cba907bf5f92ea760
leon chepkwony <leonchepkwony2@gmail.com>
2023-09-22 13:05:27 +0300

    added public method update in square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -23,6 +23,31 @@ class Square(Rectangle):
         self.width = size
         self.height = size
 
+    def update(self, *args, **kwargs):
+        """Update the Square's attributes based on arguments and keyword arguments."""
+
+        if args:
+            # Check if there are arguments and assign them accordingly
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            # If no *args, use **kwargs if provided
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = value
+                elif key == 'size':
+                    self.size = value
+                elif key == 'x':
+                    self.x = value
+                elif key == 'y':
+                    self.y = value
+
     def __str__(self):
         """Return a string representation of the Square instance."""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
