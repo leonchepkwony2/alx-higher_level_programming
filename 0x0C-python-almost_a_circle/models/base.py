bd1d1e65964c4d9d372a0deb09d0eb2573a587a5
leon chepkwony <leonchepkwony2@gmail.com>
2023-09-22 13:26:13 +0300

    added to_json static method to base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,7 +25,7 @@ class Base:
         """Static method to convert
         list of dictionaries to JSON string
         """
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
