815c59fb9b29ed6cec658106e3ce032060b0a225
leon chepkwony <leonchepkwony2@gmail.com>
2023-09-22 12:00:53 +0300

    changed variable names

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,10 +13,10 @@ class Rectangle(Base):
         super().__init__(id)
 
         # private instance attributes
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
